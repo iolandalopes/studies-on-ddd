6f21aed0926f02b61228387d9f28c0f65aaf29a0
Iolanda Lopes <iolandalopes@MacBook-Air-de-Iolanda.local>
2026-01-08 11:03:24 -0300

    adds business rules and validation

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -2,30 +2,47 @@ class Customer {
   private _id: string;
   private _name: string;
   private _address: string;
+  private _active: boolean = true;
 
-  constructor(id: string, name: string, address: string) {
+  constructor(id: string, name: string, address: string, active: boolean = true) {
     this._id = id;
     this._name = name;
     this._address = address;
+    this._active = active;
+
+    this.validate();
   }
 
-  get id(): string {
-    return this.id;
+  validate(): boolean {
+    if (this._id.length === 0) {
+      throw new Error("ID is required");
+    }
+
+    if (this._name.length === 0) {
+      throw new Error("Name is required");
+    }
+
+    if (this._address.length === 0) {
+      throw new Error("Address is required");
+    }
+
+    return true;
   }
 
-  get name(): string {
-    return this.name;
-  }
-
-  get address(): string {
-    return this.address;
-  }
-
-  set name(name: string) {
+  changeName(name: string): void {
     this._name = name;
+    this.validate();
   }
 
-  set address(address: string) {
-    this._address = address;
-  } 
+  activate(): void {
+    if (this._address.length === 0) {
+      throw new Error("Address is required to activate a customer");
+    }
+
+    this._active = true;
+  }
+
+  deactivate(): void {
+    this._active = false;
+  }
 }
